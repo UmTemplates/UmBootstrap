9fd7e9f132a5c4bb04162c2208c4a40f6420ad93
Dean Leigh <dean.leigh@wholething.co.uk>
2023-08-14 11:48:24 +0100

    Improve Conditional Logic for Block Groups (get Group name) #156

---
+++ Umbootstrap.Web/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Umbootstrap.Web.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static Dictionary<string, string> GetBlocksWithGroupNames(this IPublishedContent contentItem, string gridAlias)
+        {
+            var blocks = new Dictionary<string, string>();
+
+            var contentProperty = contentItem.Properties.FirstOrDefault(x => x.Alias == gridAlias);
+            if (contentProperty == null) return blocks;
+
+            var config = contentProperty.PropertyType.DataType.Configuration as BlockGridConfiguration;
+            if (config == null || config.BlockGroups == null) return blocks;
+
+            var blockGroupDictionary = config.BlockGroups.ToDictionary(item => item.Key.ToString(), item => item.Name);
+
+            foreach (var item in config.Blocks)
+            {
+                blocks.Add(item.ContentElementTypeKey.ToString(), blockGroupDictionary.GetValue(item.GroupKey));
+            }
+
+            return blocks;
+        }
+    }
+}
