6c488b6ead083ae925a3e012a457fa84fa510307
Dean Leigh <dean.leigh@wholething.co.uk>
2025-06-20 10:57:07 +0200

    Disabled umbnav

--- Umbootstrap.Web/Extensions/UmbNavExtensions.cs
+++ Umbootstrap.Web/Extensions/UmbNavExtensions.cs
@@ -1,30 +1,30 @@
-using Umbraco.Cms.Core.Models.PublishedContent;
-using Umbraco.Community.UmbNav.Core.Models;
+//using Umbraco.Cms.Core.Models.PublishedContent;
+//using Umbraco.Community.UmbNav.Core.Models;
 
-namespace Umbootstrap.Web.Extensions;
+//namespace Umbootstrap.Web.Extensions;
 
-public static class UmbNavExtensions
-{
-    public static bool IsActive(this UmbNavItem item,
-        IPublishedContent? currentPage, int minLevel)
-    {
-        var contentKey = item.ContentKey ?? item.Content?.Key;
+//public static class UmbNavExtensions
+//{
+//    public static bool IsActive(this UmbNavItem item,
+//        IPublishedContent? currentPage, int minLevel)
+//    {
+//        var contentKey = item.ContentKey ?? item.Content?.Key;
         
-        if (contentKey is null || currentPage is null) return false;
+//        if (contentKey is null || currentPage is null) return false;
         
-        var key = currentPage.Key;
+//        var key = currentPage.Key;
         
-        if (contentKey.Value == key)
-        {
-            return true;
-        }
+//        if (contentKey.Value == key)
+//        {
+//            return true;
+//        }
 
-        if (currentPage.Level > minLevel)
-        {
-            return currentPage.Ancestors().Any(x => x.Level >= minLevel
-            && x.Key == contentKey.GetValueOrDefault());
-        }
+//        if (currentPage.Level > minLevel)
+//        {
+//            return currentPage.Ancestors().Any(x => x.Level >= minLevel
+//            && x.Key == contentKey.GetValueOrDefault());
+//        }
         
-        return false;
-    }
-}
+//        return false;
+//    }
+//}
