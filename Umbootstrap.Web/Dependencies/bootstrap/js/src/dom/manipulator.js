29123141fa3bbac86b95b7f01d69eac0999e8e11
Dean Leigh <dean.leigh@deanleigh.co.uk>
2025-07-09 12:17:26 +0100

    Upgrade-Bootstrap-from-v5.2.2-to-v5.3.7

--- Umbootstrap.Web/Dependencies/bootstrap/js/src/dom/manipulator.js
+++ Umbootstrap.Web/Dependencies/bootstrap/js/src/dom/manipulator.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v5.2.2): dom/manipulator.js
+ * Bootstrap dom/manipulator.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
@@ -56,7 +56,7 @@ const Manipulator = {
 
     for (const key of bsKeys) {
       let pureKey = key.replace(/^bs/, '')
-      pureKey = pureKey.charAt(0).toLowerCase() + pureKey.slice(1, pureKey.length)
+      pureKey = pureKey.charAt(0).toLowerCase() + pureKey.slice(1)
       attributes[pureKey] = normalizeData(element.dataset[key])
     }
 
